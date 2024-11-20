9e42735201142f8c625dff726c6fc6e61e1145ff
ValentinoLambert <160715924+ValentinoLambert@users.noreply.github.com>
2024-11-20 15:12:20 +0100

    maj film

--- test/activeRecord/TestFilm.java
+++ test/activeRecord/TestFilm.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestFilm {
 
     @BeforeEach
-    void setUp() throws SQLException {
+    void setUp() throws SQLException, RealisateurAbsentException {
         Film.deleteTable();
         Personne.deleteTable();
         Personne.createTable();
