c7888031be6c234e4032decb01ca358c3c338b78
ValentinoLambert <160715924+ValentinoLambert@users.noreply.github.com>
2024-11-20 08:21:12 +0100

    ajout de personne et TestPersonne à vérifier avec la connection à la bd

---
+++ src/Test/TestPersonne.java
@@ -0,0 +1,65 @@
+package Test;
+
+import activeRecord.Personne;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPersonne {
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        Personne.createTable();
+        new Personne("Durand", "Paul").save();
+        new Personne("Martin", "Anne").save();
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        Personne.deleteTable();
+    }
+
+    @Test
+    void testFindAll() throws SQLException {
+        assertEquals(2, Personne.findAll().size());
+    }
+
+    @Test
+    void testFindById() throws SQLException {
+        Personne p = Personne.findById(1);
+        assertNotNull(p);
+        assertEquals("Durand", p.nom);
+    }
+
+    @Test
+    void testFindByName() throws SQLException {
+        assertEquals(1, Personne.findByName("Martin").size());
+    }
+
+    @Test
+    void testSaveNew() throws SQLException {
+        Personne p = new Personne("Dupont", "Claire");
+        p.save();
+        assertNotEquals(-1, p.id);
+        assertEquals(3, Personne.findAll().size());
+    }
+
+    @Test
+    void testUpdate() throws SQLException {
+        Personne p = Personne.findById(1);
+        p.nom = "Lemoine";
+        p.save();
+        assertEquals("Lemoine", Personne.findById(1).nom);
+    }
+
+    @Test
+    void testDelete() throws SQLException {
+        Personne p = Personne.findById(1);
+        p.delete();
+        assertNull(Personne.findById(1));
+    }
+}
---
+++ src/activeRecord/Personne.java
@@ -0,0 +1,127 @@
+package activeRecord;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.id = -1;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    private Personne(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public static void createTable() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "CREATE TABLE IF NOT EXISTS personne (id INT AUTO_INCREMENT, nom VARCHAR(40), prenom VARCHAR(40), PRIMARY KEY (id))";
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+    }
+
+    public static void deleteTable() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "DROP TABLE IF EXISTS personne";
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+    }
+
+    public static List<Personne> findAll() throws SQLException {
+        List<Personne> personnes = new ArrayList<>();
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "SELECT * FROM personne";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            personnes.add(new Personne(id, nom, prenom));
+        }
+        return personnes;
+    }
+
+    public static Personne findById(int id) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "SELECT * FROM personne WHERE id = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            return new Personne(id, nom, prenom);
+        }
+        return null;
+    }
+
+    public static List<Personne> findByName(String nom) throws SQLException {
+        List<Personne> personnes = new ArrayList<>();
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "SELECT * FROM personne WHERE nom = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, nom);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String prenom = rs.getString("prenom");
+            personnes.add(new Personne(id, nom, prenom));
+        }
+        return personnes;
+    }
+
+    public void save() throws SQLException {
+        if (this.id == -1) {
+            saveNew();
+        } else {
+            update();
+        }
+    }
+
+    private void saveNew() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "INSERT INTO personne (nom, prenom) VALUES (?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, this.nom);
+        stmt.setString(2, this.prenom);
+        stmt.executeUpdate();
+
+        ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            this.id = rs.getInt(1);
+        }
+    }
+
+    private void update() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String query = "UPDATE personne SET nom = ?, prenom = ? WHERE id = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, this.nom);
+        stmt.setString(2, this.prenom);
+        stmt.setInt(3, this.id);
+        stmt.executeUpdate();
+    }
+
+    public void delete() throws SQLException {
+        if (this.id != -1) {
+            Connection connection = DBConnection.getInstance().getConnection();
+            String query = "DELETE FROM personne WHERE id = ?";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, this.id);
+            stmt.executeUpdate();
+            this.id = -1;
+        }
+    }
+}
